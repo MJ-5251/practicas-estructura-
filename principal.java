02f53f91f94528abad543fe1a1a7a51bd0a46a42
María José <mariacordoba2301@gmail.com>
2026-05-12 08:51:46 -0500

    main

---
+++ principal.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class principal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bienvenido");
+        System.out.println("1. Gestion de clientes");
+        System.out.println("2. Gestion de vehiculos");
+        System.out.println("3. Gestion de contratos de renting");
+        System.out.println("4. Imprimir informe general");
+        System.out.println("5. Salir del sistema");
+        int opt = sc.nextInt();
+
+        switch (opt) {
+            case 1:
+                
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                
+                break;
+            case 4:
+                
+                break;
+            case 5:
+                
+                break;
+        
+            default:
+                System.out.println("opcion no valida");
+                break;
+
+        }
+    }
+    
+}
