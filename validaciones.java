6af22f93c2a1b29dbf7c08327915f89e91ce55d3
Luis Passo <luispasso2006@gmail.com>
2026-05-14 11:52:35 -0500

    HU2

---
+++ validaciones.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class validaciones {
+    
+    private String pedirCampo(String mensaje, Scanner sc) {
+        String valor;
+        do {
+            System.out.println(mensaje);
+            valor = sc.nextLine().trim();
+            if (valor.isBlank()) {
+                System.out.println("Este campo es requerido. Intente de nuevo.");
+            }
+        } while (valor.isBlank());
+        return valor;
+    }
+
+    public boolean ValidarDimension(String opt) {
+        while (opt.length() > 1) {
+            System.out.println("Solo puede agregar Y or N");
+            return false;
+
+        }
+        return true;
+
+    }
+
+    public boolean ValidarTexto(String texto) {
+        return texto.matches("[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ{} ]+");
+    }
+}
