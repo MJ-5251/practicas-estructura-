a69d5fba5e812f3fa7030b01a8dd0012c9510029
Luis Passo <luispasso2006@gmail.com>
2026-05-14 10:10:45 -0500

    HU1

--- principal.java
+++ principal.java
@@ -2,7 +2,10 @@ import java.util.Scanner;
 public class principal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        CrearEstructura c = new CrearEstructura();
         System.out.println("Bienvenido");
+        boolean salir = true;
+        while (salir) {
         System.out.println("1. Gestion de clientes");
         System.out.println("2. Gestion de vehiculos");
         System.out.println("3. Gestion de contratos de renting");
@@ -12,26 +15,26 @@ public class principal {
 
         switch (opt) {
             case 1:
-                
+                c.CrearClientes();
                 break;
             case 2:
-                
+                c.CrearVehiculo();
                 break;
             case 3:
-                
+                c.CrearContratos();
                 break;
             case 4:
-                
+                c.Informe();
                 break;
             case 5:
-                
+                salir = false;
                 break;
-        
             default:
                 System.out.println("opcion no valida");
                 break;
 
         }
     }
+}
     
 }
