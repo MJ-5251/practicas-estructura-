b5da4d0c8e52df7aec9b6b3f813ca9a754821f6c
Luis Passo <luispasso2006@gmail.com>
2026-05-15 17:38:33 -0500

    HU8

--- validaciones.java
+++ validaciones.java
@@ -27,4 +27,29 @@ public class validaciones {
     public boolean ValidarTexto(String texto) {
         return texto.matches("[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ{} ]+");
     }
+
+      public int ValidarEntero(Scanner sc) {
+        while (!sc.hasNextInt()) {
+            System.out.println("Por favor Ingrese un digito Numérico");
+            sc.nextLine();
+        }
+        return sc.nextInt();
+    }
+
+     public int ValidarCedula(Scanner sc) {
+        int cedulaE = ValidarEntero(sc);
+        String cedulaAux = cedulaE + "";
+        cedulaAux = cedulaAux.trim();
+        while (cedulaAux.length() < 7 || cedulaAux.length() > 11) {
+            System.out.println("error ingrese de 7 a 10 digitos");
+            sc.next();
+            cedulaAux = sc.next();
+        }
+        try {
+            cedulaE = Integer.parseInt(cedulaAux);
+        } catch (Exception e) {
+            System.out.println("ingrese una cedula valida");
+        }
+        return cedulaE;
+    }
 }
