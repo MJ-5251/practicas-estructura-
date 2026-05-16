336ba45b42bf1b3533f264f8e3b350e4d943b501
Luis Passo <luispasso2006@gmail.com>
2026-05-16 18:58:54 -0500

    HU6

---
+++ ExportarContratos.java
@@ -0,0 +1,34 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class ExportarContratos {
+    public void ExportarArchivo(LinkedList<ContratoRenting> VectorContratos)
+    {
+         if(VectorContratos.isEmpty())
+         {
+            System.out.println("LA LISTA ESTA VACIA NO SE PUEDE EXPORTAR EL ARCHIVO");
+            return;
+         } else{
+            try(FileWriter e = new FileWriter("Clientes.txt"))
+            {
+                for (cliente o : VectorContratos) {
+                    e.write("ID_CONTRATO: " + o.getIdContrato() + "\n");
+                    e.write("CEDULA_CLIENTE: : " + o.getCedulaCliente() + "\n");
+                    e.write("PLACA_VEHICULO: " + o.getPlacaVehiculo() + "\n");
+                    e.write("FECHA_INICIO: " + o.getFechaInicio() + "\n");
+                    e.write("FECHA_FIN: " + o.getFechaFin() + "\n");
+                    e.write("TOTAL_DIAS: " + o.getTotalDias() + "\n");
+                    e.write("VALOR_TOTAL: " + o.getValorTotal() + "\n");
+                    e.write("------------------------------\n");
+                }
+                System.out.println("ARCHIVO EXPORTADO CORRECTAMENTE ");
+            }
+            catch(IOException e)
+            {
+                e.printStackTrace();
+            }
+         }
+    }
+}
