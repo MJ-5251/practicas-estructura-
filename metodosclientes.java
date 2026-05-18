b2e2e5a70e76512c0530a6ebe2d7ae99d1dd6540
Luis Passo <luispasso2006@gmail.com>
2026-05-18 12:06:06 -0500

    HU2

--- metodosclientes.java
+++ metodosclientes.java
@@ -21,11 +21,11 @@ public class metodosclientes {
         }
         
         System.out.println("INGRESE EL NOMBRE");
-        c.setNombre(vc.ValidarNombreYApellido(sc));
+        c.setNombre(v.ValidarSoloLetras(sc, 2, 15));
         System.out.println("INGRESE EL APELLIDO");
-        c.setApellido(vc.ValidarNombreYApellido(sc));
+        c.setApellido(v.ValidarSoloLetras(sc, 2, 15));
         System.out.println("INGRESE EL TELEFONO DEL CLIENTE");
-        c.setTelefono(vc.ValidarCedulaYTelefono(sc, 10, 10));
+        c.setTelefono(vc.ValidarSoloNumeros(sc, 10, 10));
         System.out.println("INGRESE LA DIRECCION DEL CLIENTE");
         c.setDireccion(vc.ValidarDireccion(sc));
         c.setLicenciaConduccion(c.getCedula());
@@ -48,7 +48,7 @@ public class metodosclientes {
     public Stack<cliente> Modificar ( Stack<cliente> clientes, Scanner sc){
         ValidacionesClientes v = new ValidacionesClientes();
         System.out.println("Ingrese la cedula del cliente a modificar");
-        String cedula = v.ValidarCedulaYTelefono(sc, 7, 11);
+        String cedula = v.ValidarSoloNumeros(sc, 7, 11);
         Stack<cliente> aux = new Stack<>();
         boolean encontrado = false;
         while (!clientes.isEmpty()) {
@@ -56,13 +56,13 @@ public class metodosclientes {
             if (c.getCedula().equals(cedula)) {
                 encontrado = true;
                 System.out.println("Ingrese el nuevo nombre del cliente");
-                c.setNombre(v.ValidarNombreYApellido(sc));
+                c.setNombre(v.ValidarSoloLetras(sc, 2, 15));
 
                 System.out.println("Ingrese el nuevo apellido del cliente");
-                c.setApellido(v.ValidarNombreYApellido(sc));
+                c.setApellido(v.ValidarSoloLetras(sc, 2, 15));
 
                 System.out.println("Ingrese el nuevo telefono del cliente");
-                c.setTelefono(v.ValidarCedulaYTelefono(sc, 10, 10));
+                c.setTelefono(v.ValidarSoloNumeros(sc, 10, 10));
 
                 System.out.println("Ingrese la nueva direccion del cliente");
                 c.setDireccion(v.ValidarDireccion(sc));
@@ -81,7 +81,7 @@ public class metodosclientes {
     public Stack<cliente> Eliminar (Stack<cliente> clientes, Scanner sc){
         ValidacionesClientes v = new ValidacionesClientes();
         System.out.println("Ingrese la cedula del cliente a eliminar");
-        String cedula = v.ValidarCedulaYTelefono(sc, 7, 11);
+        String cedula = v.ValidarSoloNumeros(sc, 7, 11);
 
         boolean eliminado = clientes.removeIf(c -> c.getCedula().equals(cedula));
 
@@ -96,7 +96,7 @@ public class metodosclientes {
     public Stack<cliente> Buscar (Stack<cliente> clientes, Scanner sc){
         ValidacionesClientes v = new ValidacionesClientes();
         System.out.println("Ingrese la cedula del cliente a buscar");
-        String cedula = v.ValidarCedulaYTelefono(sc, 7, 11);
+        String cedula = v.ValidarSoloNumeros(sc, 7, 11);
         boolean encontrado = false;
         for (cliente c : clientes) {
             if (c.getCedula().equals(cedula)) {
