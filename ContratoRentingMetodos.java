aeedc2d5edea85fb9f0901bf2de5480d71876792
Luis Passo <luispasso2006@gmail.com>
2026-05-21 09:06:52 -0500

    HU4

--- ContratoRentingMetodos.java
+++ ContratoRentingMetodos.java
@@ -62,7 +62,7 @@ public class ContratoRentingMetodos {
         System.out.println("INGRESE EL TOTAL DE DIAS");
             c.setTotalDias(v.ValidarEntero(sc));
 
-            c.setValorTotal(v.ValidarFloat(vc.ValorTotal(c.getTotalDias(), c.getPlacaVehiculo(), VectorVehiculos)));
+            c.setValorTotal(vc.ValorTotal(vc.ValorTotal(c.getTotalDias(), c.getPlacaVehiculo(), VectorVehiculos)));
         
             c.setEstado("ACTIVO");
             lista.add(c);
@@ -100,7 +100,7 @@ public class ContratoRentingMetodos {
                 System.out.println("INGRESE EL TOTAL DE DIAS");
                 c.setTotalDias(v.ValidarEntero(sc));
 
-                c.setValorTotal(v.ValidarFloat(vc.ValorTotal(c.getTotalDias(), c.getPlacaVehiculo(), VectorVehiculos)));
+                c.setValorTotal(vc.ValorTotal(vc.ValorTotal(c.getTotalDias(), c.getPlacaVehiculo(), VectorVehiculos)));
             }
         }
         e.ExportarArchivo(lista);
